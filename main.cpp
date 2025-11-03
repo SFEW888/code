d4adf20637389abb56946aa34b397df363a20360
帝鸿 <hongmong20221953@outlook.com>
2025-11-03 23:54:45 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,262 @@
+//写一个C++程序
+//在屏幕上打印：hello world
+
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	cout << "hello world" << endl;
+//	return 0;
+// }
+
+
+
+ 
+// #include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	'a';
+//	'6';
+//	'v';
+//	"abcd";
+//	"hello world";
+//	" ";//空字符串
+//	return 0; 
+//	
+// } 
+
+
+
+
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int num;
+//	cin >> num;
+//	cout << num << endl;
+//	cout << num << endl;
+//	return 0;
+//}
+
+
+
+
+#include<iostream>
+using namespace std;
+
+//int main()
+//{
+//	float f;
+//	cin >> f;
+//	cout << f << endl;
+//	return 0;
+//}
+
+int main()
+{
+	int i = 0;
+	//cin >> i;   //这是单行注释 
+	cout << i << endl;
+	return 0;
+}
+
+
+
+/*
+#include<stdio.h>
+int main()
+{
+	int n = 0;
+	scanf("%d",&n);
+	//printf("%d\n",n);//这是单行注释 
+	return 0;
+}*/  //这是多行注释 
+
+
+
+
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//    std::cout << "hello world" << std::endl;
+//    return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+ //运行C++的程序 
+ //1.编译--F9
+ //2.运行--F10
+ //3.编译+运行--F11
+ 
+ 
+// #include<iostream>
+// using namespace std;
+// 
+// int main()
+// {
+// 	cout << "Hello,World!" << endl;
+// 	return 0;
+//  } 
+ 
+ 
+ 
+// #include<iostream>
+// using namespace std;
+// 
+// int main()
+// {
+// 	    cout << "     **     " << endl;
+//   	    cout << "     **     " << endl;
+// 	    cout << "************" << endl;
+// 		cout << "************" << endl;
+// 		cout << "    *  *    " << endl;
+// 		cout << "    *  *    " << endl;
+// 		return 0;
+// 		
+// }
+ 
+ 
+ 
+// #include<iostream>
+// using namespace stdl;
+// 
+// int main()
+// {
+// 	
+// 	
+// 	return 0;
+// }
+// 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+// #include<iostream>
+// #include<string>
+// using namespace std;
+// 
+// int main()
+// {
+// 	string s = "abc";
+// 	cout << "s:" << s << endl;
+// 	s.pop_back();
+// 	cout << "s:" << s << endl;
+// 	return 0;
+//  } 
+// 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
