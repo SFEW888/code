592261381b864b9a3947e09487c7d53346ff4d77
帝鸿 <hongmong20221953@outlook.com>
2025-11-11 21:16:23 +0800

    Add files via upload

---
+++ 竞赛8.cpp
@@ -0,0 +1,219 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	cout << "     **     " << endl;
+//	cout << "     **     " << endl;
+//	cout << "************" << endl;
+//	cout << "************" << endl;
+//	cout << "    *  *    " << endl;
+//	cout << "    *  *    " << endl;
+//	return 0;
+// } 
+
+
+
+
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	cout << "     **" << endl;
+//	cout << "     **" << endl;
+//	cout << "************" << endl;
+//	cout << "************" << endl;
+//	cout << "    *  *" << endl;
+//	cout << "    *  *" << endl;
+//	return 0;
+// }
+
+
+
+
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int n1, n2, n3;
+//	cin >> n1;
+//	cin >> n2;
+//	cin >> n3;
+//	cout << n2 << endl;
+//}
+
+
+
+
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int n1, n2, n3;
+//	cin >> n1 >> n2 >> n3;
+//	cout << n2 << endl;
+//}
+
+
+
+
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	char c = 0;
+//	cin >> c;
+//	cout << "  " << c << endl;
+//	cout << " " << c << c << c << endl;
+//	cout << c << c << c << c << c << endl;
+//	return 0;
+// } 
+
+
+
+
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int a;
+//	cin >> a;
+//	cout << a;
+//	return 0;
+//}
+
+
+
+
+
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int n = 0;
+//	cin >> n;
+//	char ch = n;
+//	cout << ch << endl;
+//	return 0;
+// } 
+
+
+
+
+
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	char ch ;
+//	cin >> ch;
+//	cout << ch << endl;
+//	return 0;
+// } 
+
+
+
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+// {
+//	int a, b, c;
+//	//ÊäÈë
+//	cin >> a >> b >> c;
+//	//Êä³ö
+//	cout << c << b << a;
+//	return 0; 
+// } 
+
+
+
+
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	int n = 0;
+//	short s = 0;
+//	cout << sizeof(n) << " " << sizeof(s) << endl;
+//	return 0;
+// } 
+
+
+
+
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int n = 0;
+//	cin >> n;
+//	cout << n * 100 << endl;
+//	return 0;
+//}
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+	cin >> a >> b;
+	cout << a + b << endl;
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
