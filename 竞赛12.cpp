9b5a4d25188e681d66c69607e1dee1e4e76582b9
帝鸿 <hongmong20221953@outlook.com>
2025-11-14 23:14:03 +0800

    Add files via upload

---
+++ 竞赛12.cpp
@@ -0,0 +1,108 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int i = 1;
+//	while(i <= 5)
+//	{
+//		cout << i << " ";
+//		i++;
+//	}
+//	return 0;
+// } 
+
+
+
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	//也可以这样写for(int i = 1;i <= 10;i++) 
+//	int i = 0; 
+//	for(i=1;i<=10;i++)
+//	{
+//		cout << i << " "
+//	}
+//	cout << i << endl; 
+//	//循环外和循环内创建不同的是循环里的使用不了，循环外的可以使用 
+//	return 0;
+// } 
+
+
+
+//求和2 
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int i = 0;
+//	int sum = 0;
+//	for(i=1;i<=100;i++)
+//	{
+//		if(n % 3 == 0)
+//		{
+//			sum += i;
+//		}
+//	}
+//	return 0;
+//}
+
+
+
+
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int i = 0;
+//	do
+//	{
+//		cout << i << " ";
+//		i++;
+//	}while(i <= 10);
+//	return 0;
+//}
+
+
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int i = 1;
+	while(i <= 10)
+	{
+		if(i == 5)
+		   continue;
+		   
+		cout << i << " ";
+		i++;   
+	}
+	return 0;
+ } 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
