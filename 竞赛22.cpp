bb9fe4e093bf7564fba8b9638147dd651bc9d087
帝鸿 <hongmong20221953@outlook.com>
2025-11-28 23:26:51 +0800

    Add files via upload

---
+++ 竞赛22.cpp
@@ -0,0 +1,61 @@
+//#include <iostream>
+//using namespace std;
+//
+//int Letters;
+//int Digits;
+//int Others;
+//int main()
+//{
+//	int ch;
+//	while((ch = getchar()) != '?')
+//	{
+//		if((ch >= 'a'&&ch <= 'z') || (ch >='A'&&ch <= 'Z'))
+//			Letters++;
+//		
+//		else if(ch >= '0'&&ch <= '9')
+//	    	Digits++;
+//		
+//		else
+//		    Others++;
+//	}
+//    cout << Letters << endl;
+//    cout << Digits << endl;
+//    cout << Others << endl;
+//    
+//    return 0;
+//
+// } 
+
+
+
+
+#include <iostream>
+using namespace std;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
