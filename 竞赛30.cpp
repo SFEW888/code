5531b2184b06d755e26d56b9012599890fb15c72
帝鸿 <hongmong20221953@outlook.com>
2026-01-02 17:02:49 +0800

    Add files via upload

---
+++ 竞赛30.cpp
@@ -0,0 +1,220 @@
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	int n = 0;
+//	short s = 0;
+//	cout << sizeof(n) << " " << sizeof(s) << endl;
+//	return 0;
+// } 
+
+
+//鸡兔同笼 
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int j = 0;//鸡
+//	int t = 0;//兔
+//	int h = 35;//头
+//	int f = 94;//脚
+//	t = (f - h * 2) / 2;
+//	j = h - t;
+//	cout << t << " " << j << endl;
+//	return 0;
+//}
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int j = 0;//鸡
+//	int t = 0;//兔
+//	int h = 35;//头
+//	int f = 94;//脚
+//	j = (4 * h - f) / 2;
+//	t = h - j;
+//	cout << t << " " << j << endl;
+//	return 0;
+//}
+
+
+
+//计算 (a+b)×c 的值
+//#include <iostream>
+//using namespace std;
+//int a, b, c;
+//int main()
+//{
+//    cin >> a >> b >> c;
+//    int r = (a + b) * c;
+//    cout << r << endl;
+//    return 0;
+//}
+
+
+
+////带余除法
+//#include <iostream>
+//using namespace std;
+//int a, b;
+//    
+//int main() 
+//{
+//    cin >> a >> b;
+//    cout << a / b << " " << a % b << endl;
+//}
+
+
+
+//#include <iostream>
+//using namespace std;
+//    
+//int main() 
+//{
+//    int time;
+//    cin >> time;
+//    cout << time / 60 / 60 << " " << time / 60 % 60 << " " << time % 60 << endl; 
+//    return 0;
+//}
+
+
+
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int a, b, c, d;
+//	cin >> a >> b >> c >> d;
+//	int t = c * 60 + d - a * 60 - b;
+//	int h, m;
+//	h = t / 60;
+//	m = t % 60;
+//	cout << h << " " << m << endl;
+//	return 0;
+//}
+
+
+
+//#include <iostream>
+//using namespace std;
+//int a,b,c;
+//int ret;
+//int main()
+//{
+//    cin >> a >> b >> c;
+//    ret = (int)(a * 0.2 + b * 0.3 + c * 0.5);//这里进行了强制类型转换，如果不转换，也会自动转换的 
+//    cout << ret << endl;
+//    return 0;
+//}
+
+
+
+//#include<cstdio>
+//int main()
+//{
+//    int a, b;
+//    scanf("%d%d", &a, &b);
+//    printf("%.3lf%%\n", b * 100.0 / a);
+//    return 0;
+//}
+//#include<cstdio>
+//int main()
+//{
+//    int a, b;
+//    scanf("%d%d", &a, &b);
+//    printf("%.3lf%%\n", b / a * 100);
+//    return 0;
+//}
+
+
+
+//#include <iostream>
+//#include <cstdio>
+//using namespace std;
+//int main()
+//{
+//    int a, b, c;
+//    cin >> a >> b >> c;
+//    printf("%8d %8d %8d\n", a, b, c);
+//    return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
