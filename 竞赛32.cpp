10538ef0cb92960873e43b7d63eaba2256d8acff
帝鸿 <hongmong20221953@outlook.com>
2026-01-06 23:00:13 +0800

    Add files via upload

--- 竞赛32.cpp
+++ 竞赛32.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 
-//尾插
-void push_back(int x)
-{
-	a[++n] = x;
- } 
+////尾插
+//void push_back(int x)
+//{
+//	a[++n] = x;
+// } 
+
+
+
+////头插
+//void push_front(int x)
+//{
+//	//要把所有的元素全部右移一位，然后放到头部位置 
+//	for(int i = n; i >= 1; i--)
+//	{
+//		a[i + 1] = a[i];
+//	}
+//	a[1] = x;//把x放在首位 
+//	n++;//不要忘记总个数 +1 
+// } 
