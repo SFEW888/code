1e5b0641dbfb39ef9e83a988d9dd8d932f88a9c4
帝鸿 <hongmong20221953@outlook.com>
2026-04-14 23:49:06 +0800

    Add files via upload

---
+++ 每日练习2.cpp
@@ -0,0 +1,115 @@
+//1.Phone numbers
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int n; string s;
+//	cin >> n >> s;
+//	
+//	if(n % 2)//ÆæÊý 
+//	{
+//		for(int i = 0; i < n; i++)
+//		{
+//			cout << s[i];
+//			if(i % 2 && i < n - 3) cout << '-';
+//		}
+//	}
+//	else
+//	{
+//		for(int i = 0; i < n; i++)
+//		{
+//			cout << s[i];
+//			if(i % 2 && i < n - 2) cout << '-';
+//	    }
+//    }
+//    return 0;
+//}
+
+
+
+
+//2.zzc ÖÖÌï
+//#include <iostream>
+//
+//using namespace std;
+//
+//typedef long long LL;
+//
+//int main()
+//{
+//	LL x, y; cin >> x >> y;
+//	
+//	LL ret = 0;
+//	while(x && y)
+//	{
+//		LL cnt = x / y;
+//		ret += cnt * y * 4;
+//		x %= y;
+//		swap(x, y);
+//	}
+//	
+//	cout << ret << endl;
+//	
+//	return 0;
+//}
+
+
+
+
+//3.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
