3ffac3bbbcbdb00aa30272051beda66f473d8f95
帝鸿 <hongmong20221953@outlook.com>
2026-04-14 23:48:25 +0800

    Add files via upload

--- 每日练习1.cpp
+++ 每日练习1.cpp
@@ -211,54 +211,124 @@
 
 
 //6.混合牛奶 Mixing Milk
-#include <iostream>
-#include <algorithm>
-
-using namespace std;
-
-const int N = 5010;
-
-int n, m;
-struct node
-{
-	int p, x;
-}a[N]; 
-
-bool cmp(node& a, node& b)
-{
-	return a.p < b.p;
-}
-
-int main()
-{
-	cin >> n >> m;
-	for(int i = 1; i <= m; i++) cin >> a[i].p >> a[i].x;
-	
-	sort(a + 1, a + 1 + m, cmp);
-	
-	int ret = 0, sum = 0;
-	for(int i = 1; i <= m; i++)
-	{
-		int t = min(a[i].x, n - sum);
-		ret += t * a[i].p;
-		sum += t;
-	}
-	
-	cout << ret << endl;
-}
-
-
+//#include <iostream>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//const int N = 5010;
+//
+//int n, m;
+//struct node
+//{
+//	int p, x;
+//}a[N]; 
+//
+//bool cmp(node& a, node& b)
+//{
+//	return a.p < b.p;
+//}
+//
+//int main()
+//{
+//	cin >> n >> m;
+//	for(int i = 1; i <= m; i++) cin >> a[i].p >> a[i].x;
+//	
+//	sort(a + 1, a + 1 + m, cmp);
+//	
+//	int ret = 0, sum = 0;
+//	for(int i = 1; i <= m; i++)
+//	{
+//		int t = min(a[i].x, n - sum);
+//		ret += t * a[i].p;
+//		sum += t;
+//	}
+//	
+//	cout << ret << endl;
+//}
 
 
 
 
+//7.开心的金明
+//#include <iostream>
+//
+//using namespace std;
+//
+//const int N = 2010, M = 30;
+//
+//int n, m;
+//int v[M], p[M];
+//int f[N];
+//
+//int main()
+//{
+//	cin >> n >> m;
+//	for(int i = 1; i <= m; i++) cin >> v[i] >> p[i];
+//	
+//	for(int i = 1; i <= m; i++)
+//	    for(int j = n; j >= v[i]; j--)
+//	        f[j] = max(f[j], f[j - v[i]] + v[i] * p[i]);
+//	
+//	cout << f[n] << endl;        
+//} 
 
 
 
 
-
-
-
+//借教室 
+//#include <iostream>
+//
+//using namespace std;
+//
+//const int N = 1e6 + 10;
+//
+//int n, m;
+//int r[N];
+//int d[N], s[N], t[N];
+//
+//int f[N];
+//
+//bool check(int x)
+//{
+//	//初始化差分数组
+//	for(int i = 1; i <= n; i++)
+//	{
+//		f[i] = r[i] - r[i - 1];
+//	}
+//	
+//	//处理订单
+//	for(int i = 1; i <= x; i++)
+//	{
+//		f[s[i]] -= d[i];
+//		f[t[i] + 1] += d[i]; 
+//	} 
+//	
+//	for(int i = 1; i <= n; i++)
+//	{
+//		f[i] = f[i - 1] + f[i];
+//		if(f[i] < 0) return false;
+//	}
+//	return true;
+//}
+//
+//int main()
+//{
+//	cin >> n >> m;
+//	for(int i = 1; i <= n; i++) cin >> r[i];
+//	for(int i = 1; i <= m; i++) cin >> d[i] >> s[i] >> t[i];
+//	
+//	int l = 1, r = m;
+//	while(l < r)
+//	{
+//		int mid = (l + r) / 2;
+//		if(check(mid)) l = mid + 1;
+//		else r = mid;
+//	}
+//	
+//	if(check(l)) cout << 0 << endl;
+//	else cout << -1 << endl << l << endl;
+//}
 
 
 
